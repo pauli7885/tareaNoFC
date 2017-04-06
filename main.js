1c85884841175fdad8e649c70c60142542d70e90
Paulianalysall <pisalazart@gmail.com>
2017-04-06 17:51:46 -0300

    inicio tarea

---
+++ main.js
@@ -0,0 +1,6 @@
+var A=["Enero", "Febrero", "Marzo","Abril","Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+var B=prompt("Ingresa el mes de tu cumpleaños")
+for(var i=0; i<A.length; i++){
+  if(A[i]!=B)
+    console.log(A[i])}
+  
